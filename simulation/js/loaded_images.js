ffbf03af186ea2fe41f27a6874c5a616843fb0e8
vleadadmin <admin@vlabs.ac.in>
2023-08-11 10:20:59 +0000

    https://virtual-labs.github.io/virtual-labs/exp-characteristics-of-crystallization-under-different-conditions-iitb click on the link to test your code.

---
+++ simulation/js/loaded_images.js
@@ -0,0 +1,8 @@
+// all image components
+var beaker = new Image();
+beaker.src = './images/WEBP/Beaker.webp';
+var magnetic_stirrer = new Image();
+magnetic_stirrer.src = './images/WEBP/MagneticStirrer.webp';
+var thermometer = new Image();
+thermometer.src = './images/WEBP/Thermometer.webp';
+//# sourceMappingURL=loaded_images.js.map
